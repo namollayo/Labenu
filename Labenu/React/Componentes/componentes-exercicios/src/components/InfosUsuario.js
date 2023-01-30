0146403656aeb8e4936e82b48de6ebd4ed567040
namollayo <gabisantcarrara@gmail.com>
2023-01-30 18:47:26 -0300

    add Components guided & fixation practice and exercises done

---
+++ Labenu/React/Componentes/componentes-exercicios/src/components/InfosUsuario.js
@@ -0,0 +1,8 @@
+export function InfoUsuario() {
+    return (
+        <div className="userInfo">
+            <p>Nome de Usuário</p>
+            <img src="https://picsum.photos/400/400?a=1" alt="User profile pic" className="userInfoPhoto" />
+        </div>
+    )
+}
---
+++ Labenu/React/Componentes/componentes-template/template/src/Componentes/Garagem.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {Carro} from "./Carro"
+
+
+export default function Garagem() {
+    return (
+    <div>
+        <h1>Garagem do Murillo</h1> 
+        <Carro />
+
+    </div>  
+    )  
+ }
